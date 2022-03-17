21d420269aa1b8c050e5dc376a14c92974432791
Tochjosh <tochjosh@gmail.com>
2022-03-18 00:39:51 +0100

    added over effect unto slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -48,7 +48,7 @@ const Carousel = styled(slider)`
 
     }
 
-    li.slick-active button::before {
+    li.slick-active button:before {
         color: white;
     }
     .slick-list {
@@ -61,6 +61,7 @@ const Carousel = styled(slider)`
 `
 
 const Wrap = styled.div`
+cursor: pointer;
 img {
     border: 4px solid transparent;
     width: 100%;
@@ -68,6 +69,11 @@ img {
     border-radius: 4px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px 10px;
+    transition-duration: 300ms;
+
+    &:hover {
+        border: 4px solid rgba(249, 249, 249, 0.8);
+    }
 }
 
 `
