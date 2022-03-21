9728f42780cbdd115abe187080328503a6de9dba
Tochjosh <tochjosh@gmail.com>
2022-03-21 23:05:57 +0100

    deail page done with background image

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Detail() {
   return (
-    <div>Detail</div>
+    <div>
+      <Background>
+        <img src='skull.jpeg' alt='skull' />
+      </Background>
+      <ImageTitle>
+        <img src='' alt='' />
+      </ImageTitle>
+    </div>
   )
 }
 
 export default Detail
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+const ImageTitle = styled.div`
+
+`
